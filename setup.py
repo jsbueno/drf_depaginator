b6bb90af6ac0cee11e4f467f668d5a1086de48de
João S. O. Bueno <gwidion@gmail.com>
2016-01-22 18:49:44 -0200

    version bump

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name = 'drf_depaginator',
-    version = "0.1",
+    version = "0.2",
     package_dir = { '': 'src'},
     license = "APACHE",
     author = "João S. O. Bueno",
